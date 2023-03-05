df3b69932291bcaec0f815900926bf28f1342ea5
wild8ear <wild8ear@yandex.ru>
2023-03-05 23:37:04 +0300

    3. Сокобан. Тестовое задание. [#75283 #397476]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,38 +3,30 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        if (hasWinHorizontal(board) || hasWinVertical(board)) {
-            rsl = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1 && (hasWinHorizontal(board, index) || hasWinVertical(board, index))){
+                rsl = true;
+            }
         }
         return rsl;
     }
 
-    public static boolean hasWinHorizontal(int[][] array) {
+    public static boolean hasWinHorizontal(int[][] array, int row) {
         boolean rsl = true;
-        for (int i = 0; i < array.length; i++) {
-            rsl = true;
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j] != 1) {
-                    rsl = false;
-                }
-            }
-            if (rsl) {
+        for (int index = 0; index < array[row].length; index++) {
+            if (array[row][index] != 1) {
+                rsl = false;
                 break;
             }
         }
         return  rsl;
     }
 
-    public static boolean hasWinVertical(int[][] array) {
+    public static boolean hasWinVertical(int[][] array, int column) {
         boolean rsl = true;
-        for (int i = 0; i < array.length; i++) {
-            rsl = true;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j][i] != 1) {
-                    rsl = false;
-                }
-            }
-            if (rsl) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index][column] != 1) {
+                rsl = false;
                 break;
             }
         }
